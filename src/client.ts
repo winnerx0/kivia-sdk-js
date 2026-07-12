00e5391b47215441d7494e934e0f1748f9e4d9a8
winnerx0 <eragbonikpeya@gmail.com>
2026-07-12 08:48:26 +0100

    chore: update default base URL to kivia.pxxl.click

--- src/client.ts
+++ src/client.ts
@@ -6,7 +6,7 @@ export class KiviaClient {
 
   constructor(options: KiviaClientOptions) {
     this.apiKey = options.apiKey;
-    this.baseUrl = options.baseUrl || 'https://kivia.winnerx0.dev/api/v1';
+    this.baseUrl = options.baseUrl || 'https://kivia.pxxl.click/api/v1';
   }
 
   /**
